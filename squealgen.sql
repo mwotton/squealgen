322d9c79ec151f677f9951dd6567d1f6808d865c
Mark Wotton <mwotton@gmail.com>
2020-02-03 17:40:47 -0500

    working, ish. needs constraints still

--- squealgen.sql
+++ squealgen.sql
@@ -1,10 +1,21 @@
 
-\set QUIET   
+\set QUIET
+\set ON_ERROR_STOP true
+
 set search_path to information_schema;
--- imports of DOOM
+\echo -- | This code was originally created by squealgen. Edit if you know how it got made and are willing to own it now.
+
+create or replace function croak(message text) returns text as $$
+begin
+  raise 'Croaked: %', message;
+end;
+$$
+LANGUAGE plpgsql;
+
+-- PRAGMAS of DOOM
 \echo {-# LANGUAGE DataKinds #-}
 \echo {-# LANGUAGE DeriveGeneric #-}
-\echo {-# LANGUAGE OverloadedLabels #-}
+\echo {-# LANGUAGE OverloadedLabels #-} 
 \echo {-# LANGUAGE FlexibleContexts #-}
 \echo {-# LANGUAGE OverloadedStrings  #-}
 \echo {-# LANGUAGE TypeApplications #-}
@@ -13,13 +24,65 @@ set search_path to information_schema;
 
 \echo module :modulename where
 \echo import Squeal.PostgreSQL
+
+-- squeal doesn't yet support cidr or ltree, so let's emit them explicitly
+\echo type PGcidr = UnsafePGType "cidr"
+\echo type PGltree = UnsafePGType "ltree"
+
+-- now we emit all the enumerations
+
+with enumerations as (select distinct on (udt_name,enum_values)
+  'type PG' || udt_name || ' = ''PGenum ' || E'\n' || '  ''[' || enum_values || ']' as line
+from (select col.udt_name,
+       string_agg('"' ||enu.enumlabel || '"', ', ' 
+                  order by enu.enumsortorder) as enum_values
+from information_schema.columns col
+join information_schema.tables tab on tab.table_schema = col.table_schema
+                                   and tab.table_name = col.table_name
+                                   and tab.table_type = 'BASE TABLE'
+join pg_type typ on col.udt_name = typ.typname
+join pg_enum enu on typ.oid = enu.enumtypid
+where col.table_schema not in ('information_schema', 'pg_catalog')
+      and typ.typtype = 'e'
+group by col.table_schema,
+         col.table_name,
+         col.ordinal_position,
+         col.column_name,
+         col.udt_name
+order by col.table_schema,
+         col.table_name,
+         col.ordinal_position) sub)
+select string_agg(enumerations.line, E'\n') as enums	from enumerations \gset
+
+\echo :enums
+
 with   mytables as (SELECT tables.*,
                          initcap(tables.table_name) as cappedName,
-			 '[]'::text as haskCols,
-			 '[]'::text as constraintCols,			 
-                         json_object_agg (columns.column_name, columns.*) as columns
-FROM columns
-join tables on columns.table_name = tables.table_name
+			 E'\n' || '  ''[' || string_agg(mycolumns.colDef, E'\n' || '  ,') || ']' as haskCols,
+			 '[]'::text as constraintCols,
+			 array_agg(mycolumns.colDef) as columns
+--                         json_object_agg (mycolumns.column_name, mycolumns.*) as columns
+FROM (select columns.*,
+           ('"' || column_name || '"' || ' ::: ' || case when column_default is null then '''Def'    else '''NoDef' end ||
+           ' :=> ' ||
+	      (case is_nullable
+	         when 'YES' then '''Null'
+	         when  'NO' then '''NotNull'
+--   	         else (1/0) :: text
+	         else croak ('is_nullable broken somehow: ' || is_nullable)
+		 end )	
+		 
+	   -- mildly tricky: standard postgresql datatypes need a tick, usergen types don't. HOWEVER! if we leave them off, we just
+	   -- get warnings, so this might be something to fix later.
+	   || ' '
+	   || (case 
+	      -- this won't work for everything - should check if it's got a max length.
+	        when udt_name = 'varchar' then 'PGtext'
+                else ('PG' || (udt_name :: text))
+		end))
+		as colDef
+  from columns) mycolumns
+join tables on mycolumns.table_name = tables.table_name
 WHERE table_type = 'BASE TABLE'
   AND tables.table_schema NOT IN ('pg_catalog', 'information_schema')
 group by tables.table_catalog,
@@ -34,7 +97,7 @@ group by tables.table_catalog,
 	 tables.is_insertable_into,
 	 tables.is_typed,
 	 tables.commit_action)
-select string_agg('type ' || mytables.cappedName || 'Columns = ''' || mytables.haskCols, E'\n') as cols,
+select string_agg('type ' || mytables.cappedName || 'Columns = ' || mytables.haskCols, E'\n') as cols,
        string_agg('type ' || mytables.cappedName || 'Constraints = ''' || mytables.constraintCols, E'\n') as straints,
        string_agg('type ' || mytables.cappedName || 'Table = ' || mytables.cappedName || 'Constraints :=> ' || mytables.cappedName || 'Columns', E'\n') as tabs
 from mytables \gset
