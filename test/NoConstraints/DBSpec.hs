cf11c53530613d297fe10945786d4831e1e87af3
Mark Wotton <mwotton@gmail.com>
2020-05-09 19:38:31 -0400

    add failing test case for no-constraint schemas

---
+++ test/NoConstraints/DBSpec.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DeriveAnyClass #-}
+module NoConstraints.DBSpec where
+
+import qualified Generics.SOP as SOP
+import qualified GHC.Generics as GHC
+import Test.Hspec
+import NoConstraints.Schema
+import Squeal.PostgreSQL
+
+data Foo = Foo { name :: String }
+  deriving stock (Show, GHC.Generic, Eq)
+  deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+
+getFoos :: Statement DB () Foo
+getFoos = query $ select_ #name (from $ table #foos)
+
+spec = describe "NoConstraints" $ do
+  it "compiles" $ do
+    -- nothing to do on an empty database
+    'a' `shouldBe` 'a'
