474e5e8407e64a649f7cc00bea5e2944246a0bd0
Mark Wotton <mwotton@gmail.com>
2020-05-14 12:15:50 -0400

    generate types for domains, test to demonstrate pulling them out as the result of a function

--- test/Domains/DBSpec.hs
+++ test/Domains/DBSpec.hs
@@ -14,17 +14,31 @@ import qualified GHC.Generics      as GHC
 import           Squeal.PostgreSQL
 import           Test.Hspec
 
-validInsert :: Statement DB (Only Int64) ()
+
+newtype PositiveInt = PositiveInt { unPositive :: Int64 }
+  deriving stock (Eq,Show,GHC.Generic)
+  deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+
+
+validInsert :: Statement DB PositiveInt ()
 validInsert = manipulation $
   insertInto_ #pluslove
     (Values_ (Set (param @1) `as` #num))
 
+-- FIXME this should return (Only (Maybe PositiveInt)), waiting for help from eitan
+increment :: Statement DB PositiveInt (Only (Maybe Int64))
+increment = query $ values ((function #increment_positive) (param @1) `as` #fromOnly) []
+
 spec = describe "Domains" $ do
   it "accepts a good value" $ do
     runSession "./test/Domains/Schema.dump.sql"
-      (getRows =<< executeParams validInsert (Only ((1)::Int64)))
+      (getRows =<< executeParams validInsert (PositiveInt 1))
       `shouldReturn` []
   it "rejects a bad one" $ do
     runSession "./test/Domains/Schema.dump.sql"
-      (getRows =<< executeParams validInsert (Only ((-1)::Int64)))
+      (getRows =<< executeParams validInsert (PositiveInt (-1)))
       `shouldThrow` (\(_::SquealException) -> True)
+  it "can run a function that takes and returns a domain" $ do
+    runSession "./test/Domains/Schema.dump.sql"
+      (getRows =<< executeParams increment (PositiveInt 1))
+      `shouldReturn` [Only (Just 2)]
--- test/Domains/Schema.dump.sql
+++ test/Domains/Schema.dump.sql
@@ -2,3 +2,6 @@
 CREATE DOMAIN "positive" AS int8 CHECK ((("value" > 0) AND "value" IS NOT NULL));
 
 create table pluslove ( num positive not null );
+create function increment_positive (positive) returns positive as $$
+  select ($1 + 1)::positive;
+$$ language sql strict;
